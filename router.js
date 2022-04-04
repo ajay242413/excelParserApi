5679f06190c321bf6fa8a6e5e94d3fa6bb134485
kumara163 <ajeya.kumar@emc.com>
2022-04-04 16:52:26 +0530

    updated router

--- router.js
+++ router.js
@@ -1,17 +1,9 @@
 const express =     require('express'),
     queries =       require('./queries'),
     router = express.Router(),
-    multer = require('multer'),
-    path = require('path');
+    multer = require('multer');
 
 upload = multer();
-
-router.get('/',function(req,res) {
-    res.sendFile(path.dirname(__dirname)
-        + "/restapi/index.html")
-})
-
-path.resolve('..', __dirname)
 
 
 router.get('/readData', queries.readData)
